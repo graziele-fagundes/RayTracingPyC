facc5a70e68a55c38eb5b751786fef5a5ac61005
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2024-08-10 00:02:41 -0300

    reflections

--- testlib.c
+++ testlib.c
@@ -2,9 +2,27 @@
 #include <stdlib.h>
 #include <float.h>
 
+typedef struct HitPayload {
+    float hitDistance;
+    float WorldPosition[3];
+    float WorldNormal[3];
+
+    int ObjectIndex;
+} s_hitPayload;
+
+typedef struct Ray {
+    float origin[3];
+    float direction[3];
+} s_ray;
+
 float *normalize(float *arr, int size);
-int rayTracing(float x, float y, float xLightDir);
+s_hitPayload TraceRay(s_ray ray);
+s_hitPayload ClosestHit(s_ray ray, int objectIndex, float hitDistance);
+s_hitPayload Miss(s_ray ray);
+int PerPixel(float x, float y);
 float dotProduct(float *a, float *b);
+void reflect(float *direction, float *normal, float *reflected);
+
 
 typedef struct Sphere {
     float position[3];
@@ -20,50 +38,142 @@ typedef struct Scene {
 s_scene scene = {
     .spheres = (s_sphere[]) {
         {
-            .position = {0, 0, 0},
-            .radius = 0.1,
-            .color = {255, 0, 0}
+            .position = {0, 0.025, 0},
+            .radius = 0.02,
+            .color = {255, 0, 255}
         },
         {
-            .position = {-0.1, 0, -0.1},
-            .radius = 0.1,
-            .color = {0, 255, 0}
+            .position = {0, -0.1, 0},
+            .radius = 0.08,
+            .color = {51, 77, 255}
         },
     },
     .numSpheres = 2
-}; 
+};
 
-int rayTracing(float x, float y, float xLightDir) {
-    float rayOrigin[] = {0, 0, 0.2};
+s_hitPayload ClosestHit(s_ray ray, int objectIndex, float hitDistance){
+
+    s_hitPayload hitPayload = {
+        .hitDistance = hitDistance,
+        .ObjectIndex = objectIndex
+    };
+
+    s_sphere *closestSphere = &scene.spheres[objectIndex];
     float origin[3];
-    float spherePosition[3] = {-0.1, 0, 0};
-    float hitPoint[3];
+    float lightDir[] = {-1, -1, -1};
     float normal[3];
-    float lightDir[] = {xLightDir, -1, -1};
-    float lightDirNormalized[3];
-    float rayDirection[] = {x, -y, -1.0};
     int red, green, blue;
 
-    if (scene.numSpheres == 0) {
-        red = 128;
-        green = 128;
-        blue = 128;
-        int rgb = (red << 16) | (green << 8) | blue;
-        return rgb;
+    for (int i = 0; i < 3; i++) {
+        origin[i] = ray.origin[i] - closestSphere->position[i];
     }
 
-    s_sphere *closestSphere = NULL;
+    for (int i = 0; i < 3; i++) {
+        hitPayload.WorldPosition[i] = origin[i] + ray.direction[i] * hitDistance;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        hitPayload.WorldNormal[i] = hitPayload.WorldPosition[i];
+    }
+    normalize(hitPayload.WorldNormal, 3);
+
+    for (int i = 0; i < 3; i++) {
+        hitPayload.WorldPosition[i] += closestSphere->position[i];
+    }
+
+    return hitPayload;
+}
+
+s_hitPayload Miss(s_ray ray){
+    s_hitPayload HitPayload;
+    HitPayload.hitDistance = -1;
+
+    return HitPayload;
+}
+
+int PerPixel(float x, float y){
+    // Initialize the ray
+    s_ray ray = {
+        .origin = {0, 0, 0.2},
+        .direction = {x, -y, -1.0}
+    };
+    
+    normalize(ray.direction, 3);
+
+    int colorR = 0;
+    int colorG = 0;
+    int colorB = 0;
+
+    int skyColorR = 0; // Sky blue
+    int skyColorG = 0;
+    int skyColorB = 0;
+
+    float multiplier = 1.0f;
+
+    float lightDir[] = {-1, -1, -1};
+    normalize(lightDir, 3);
+    lightDir[0] = -lightDir[0];
+    lightDir[1] = -lightDir[1];
+    lightDir[2] = -lightDir[2];
+
+    int bounces = 2;
+    for (int i = 0; i < bounces; i++){
+        s_hitPayload HitPayload = TraceRay(ray);
+
+        if (HitPayload.hitDistance < 0){
+            colorR += skyColorR * multiplier;
+            colorG += skyColorG * multiplier;
+            colorB += skyColorB * multiplier;
+            break;
+        }
+
+        float lightIntensity = fmaxf(dotProduct(HitPayload.WorldNormal, lightDir), 0.0f);
+
+        s_sphere *Sphere = &scene.spheres[HitPayload.ObjectIndex];
+        int red = Sphere->color[0];
+        int green = Sphere->color[1];
+        int blue = Sphere->color[2];
+
+        colorR += (int)(red * lightIntensity * multiplier);
+        colorG += (int)(green * lightIntensity * multiplier);
+        colorB += (int)(blue * lightIntensity * multiplier);
+
+        multiplier *= 0.7f;
+
+        for (int i = 0; i < 3; i++){
+            ray.origin[i] = HitPayload.WorldPosition[i] + HitPayload.WorldNormal[i] * 0.001f;
+        }
+
+        float reflectedDirection[3];
+        reflect(ray.direction, HitPayload.WorldNormal, reflectedDirection);
+
+        for (int i = 0; i < 3; i++){
+            ray.direction[i] = reflectedDirection[i];
+        }
+    }
+
+    colorR = fminf(fmaxf(colorR, 0), 255);
+    colorG = fminf(fmaxf(colorG, 0), 255);
+    colorB = fminf(fmaxf(colorB, 0), 255);
+
+    return (colorR << 16) | (colorG << 8) | colorB;
+}
+
+
+s_hitPayload TraceRay(s_ray ray) {
+    float origin[3];
+    int closestSphere = -1;
     float hitDistance = FLT_MAX;
 
     for (int i = 0; i < scene.numSpheres; i++){
         s_sphere sphere = scene.spheres[i];
 
-        origin[0] = rayOrigin[0] - sphere.position[0];
-        origin[1] = rayOrigin[1] - sphere.position[1];
-        origin[2] = rayOrigin[2] - sphere.position[2];
+        origin[0] = ray.origin[0] - sphere.position[0];
+        origin[1] = ray.origin[1] - sphere.position[1];
+        origin[2] = ray.origin[2] - sphere.position[2];
 
-        float a = dotProduct(rayDirection, rayDirection);
-        float b = 2 * dotProduct(rayDirection, origin);
+        float a = dotProduct(ray.direction, ray.direction);
+        float b = 2 * dotProduct(ray.direction, origin);
         float c = dotProduct(origin, origin) - pow(sphere.radius, 2);
 
         float discriminant = pow(b, 2) - 4 * a * c;
@@ -75,50 +185,21 @@ int rayTracing(float x, float y, float xLightDir) {
 
         // float t0 = (-b + sqrt(discriminant)) / (2 * a);
         float closestT = (-b - sqrt(discriminant)) / (2 * a);
-        if (closestT < hitDistance){
+        if (closestT > 0 && closestT < hitDistance){
             hitDistance = closestT;
-            closestSphere = &scene.spheres[i];
+            closestSphere = (int)i;
         }
     }
 
-    if (closestSphere == NULL){
-        red = 128;
-        green = 128;
-        blue = 128;
-        int rgb = (red << 16) | (green << 8) | blue;
-        return rgb;
+    if (closestSphere < 0){
+        return Miss(ray);
     }
 
-    origin[0] = rayOrigin[0] - closestSphere->position[0];
-    origin[1] = rayOrigin[1] - closestSphere->position[1];
-    origin[2] = rayOrigin[2] - closestSphere->position[2];
-
-    for (int i = 0; i < 3; i++) {
-        hitPoint[i] = origin[i] + rayDirection[i] * hitDistance;
-    }
-
-    for (int i = 0; i < 3; i++) {
-        normal[i] = hitPoint[i];
-    }
-    normalize(normal, 3);
-
-    normalize(lightDir, 3);
-    lightDir[0] = -lightDir[0];
-    lightDir[1] = -lightDir[1];
-    lightDir[2] = -lightDir[2];
-
-
-    float d = fmaxf(dotProduct(normal, lightDir), 0);
-
-    red = (int)((d) * closestSphere->color[0]);
-    green = (int)((d) * closestSphere->color[1]);
-    blue = (int)((d) * closestSphere->color[2]);  
-
-    // Combine RGB values into a single integer
-    int rgb = (red << 16) | (green << 8) | blue;
-
-    return rgb;
+    return ClosestHit(ray, closestSphere, hitDistance);
 }
+
+
+
 
 float *normalize(float *arr, int size) {
     float denominator = sqrt(pow(arr[0], 2) + pow(arr[1], 2) + pow(arr[2], 2));
@@ -138,4 +219,14 @@ float dotProduct(float *a, float *b) {
     }
 
     return result;
+}
+
+void reflect(float *direction, float *normal, float *reflected) {
+    // Calculate the dot product of the direction and normal
+    float dot = dotProduct(direction, normal);
+
+    // Calculate the reflected vector
+    for (int i = 0; i < 3; i++) {
+        reflected[i] = direction[i] - 2.0 * dot * normal[i];
+    }
 }
--- testlibwrapper.py
+++ testlibwrapper.py
@@ -5,8 +5,8 @@ import time
 
 # Carregar a biblioteca C
 testlib = ctypes.CDLL('C:\\msys64\\usr\\bin\\teste.dll')
-testlib.rayTracing.argtypes = [ctypes.c_float, ctypes.c_float, ctypes.c_float]
-testlib.rayTracing.restype = ctypes.c_int
+testlib.PerPixel.argtypes = [ctypes.c_float, ctypes.c_float]
+testlib.PerPixel.restype = ctypes.c_int
 
 # Função para extrair componentes RGB
 def unpack_rgb_color(color):
@@ -16,25 +16,17 @@ def unpack_rgb_color(color):
     return red, green, blue
 
 # Função para criar a imagem da esfera
-def create_high_res_sphere_image(width, height, scale, button_value):
+def create_high_res_sphere_image(width, height, scale):
     img = Image.new('RGB', (width, height))
     pixels = img.load()
     for x in range(width):
         for y in range(height):
             coord_x = scale * (x - width / 2) / width
             coord_y = scale * (y - height / 2) / height
-            color = testlib.rayTracing(coord_x, coord_y, button_value)
+            color = testlib.PerPixel(coord_x, coord_y)
             r, g, b = unpack_rgb_color(color)
             pixels[x, y] = (r, g, b)
     return img
-
-# Função para atualizar a imagem com base no valor do botão
-def update_image(button_value):
-    global image_tk, label
-    image = create_high_res_sphere_image(width, height, scale, button_value)
-    image_tk = ImageTk.PhotoImage(image)
-    label.config(image=image_tk)
-    label.image = image_tk  # Atualiza a referência para evitar que o Python faça garbage collection
 
 # Configuração da janela principal
 root = Tk()
@@ -46,32 +38,12 @@ scale = 2.0
 width, height = 1280, 960
 
 # Cria a imagem inicial
-image = create_high_res_sphere_image(width, height, scale, 0.0)
+image = create_high_res_sphere_image(width, height, scale)
 image_tk = ImageTk.PhotoImage(image)
 
 # Label para exibir a imagem
 label = Label(root, image=image_tk)
 label.pack()
-
-# Funções para cada botão
-def set_value_to_minus_one():
-    update_image(-1)
-
-def set_value_to_zero():
-    update_image(0)
-
-def set_value_to_one():
-    update_image(1)
-
-# Cria os botões e posiciona-os no canto superior direito
-button_minus_one = Button(root, text="-1", command=set_value_to_minus_one)
-button_minus_one.place(relx=0.9, rely=0.05, anchor="ne")
-
-button_zero = Button(root, text="0", command=set_value_to_zero)
-button_zero.place(relx=0.9, rely=0.1, anchor="ne")
-
-button_one = Button(root, text="1", command=set_value_to_one)
-button_one.place(relx=0.9, rely=0.15, anchor="ne")
 
 # Executa a interface gráfica
 root.mainloop()
